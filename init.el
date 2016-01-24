b234e1e0b150d06c5c8fe76f5d1cdc65c8b73c01
Kazuya NUMATA <numata@gmail.com>
2016-01-24 10:16:18 +0900

    Replace yaml-mode with el-get's one

--- init.el
+++ init.el
@@ -63,9 +63,8 @@
 (require 'magit-svn)
 
 ;; YAML mode
-(add-to-list 'load-path "~/elisp/yaml-mode")
+(el-get-bundle yaml-mode)
 ;;; yaml-mode の設定
-(require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-hook 'yaml-mode-hook
 	  '(lambda ()
