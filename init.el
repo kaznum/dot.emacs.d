40144339a03fc62edf8b3578889c2dbbb95fcd4b
Kazuya NUMATA <numata@gmail.com>
2016-01-24 10:15:20 +0900

    Define el-get settings with orthodox style

--- init.el
+++ init.el
@@ -8,10 +8,16 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/misc/elib-1.0"))
 
 ;;;;;; el-get settings
-(add-to-list 'load-path (locate-user-emacs-file "el-get"))
-(require 'el-get)
-;; el-getでダウンロードしたパッケージは ~/.emacs.d/ に入るようにする
-(setq el-get-dir (locate-user-emacs-file "el-get-managed"))
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 ;;;;;; end of el-get settings
 
 ;;jde
